ce2c8172ed42c486a03536e6d1a22afe7d1b92c9
INDRANIL SAHA <indranilsaha777@gmail.com>
2025-02-12 15:01:37 +0530

    Add files via upload

---
+++ myfunc.py
@@ -0,0 +1,14 @@
+def myfunc():
+    global a
+    a=4
+    b=6
+    result=a+b
+    print(result)
+    print(f"b is local variable : {b}")
+
+
+myfunc()
+a=a+1
+print(f"a is local variable : {a}")
+
+
