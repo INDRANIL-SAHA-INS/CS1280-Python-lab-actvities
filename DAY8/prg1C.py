3ab7a3cf7b569863888881e64df7c478db379ec2
INDRANIL-SAHA-INS <indranilsaha777@gmail.com>
2025-04-10 10:59:48 +0530

    added my lab works

--- DAY8/prg1C.py
+++
@@ -1,2 +0,0 @@
-numbers = [float(input(f"Enter number {i+1}: ")) for i in range(5)]
-print("The largest number is:", max(numbers))
--- DAY9/Program 3/1-Act.py
+++ DAY9/Program 3/1-Act.py
@@ -1,23 +1,20 @@
 def list_operations():
-    my_list = [2,4,68,11,3]
-
+    my_list = []
     while True:
         print("\nList Operations:")
         print("1. Insert an element")
         print("2. Delete an element")
         print("3. Find an element")
-        print("4. Display list")
-        print("5. Sorting")
-        print("6.Reversing")
-        print("7.Exit")
-
+        print("4. Display List")
+        print("5. Sort List")
+        print("6. Reverse List")
+        print("7. Exit")
         choice = int(input("Enter your choice: "))
-
+        
         if choice == 1:
             element = input("Enter element to insert: ")
             my_list.append(element)
             print(f"Element '{element}' inserted.")
-
         elif choice == 2:
             element = input("Enter element to delete: ")
             if element in my_list:
@@ -25,34 +22,23 @@ def list_operations():
                 print(f"Element '{element}' deleted.")
             else:
                 print(f"Element '{element}' not found.")
-
         elif choice == 3:
-            element = int(input("Enter element to find: "))
+            element = input("Enter element to find: ")
             if element in my_list:
                 print(f"Element '{element}' found.")
             else:
                 print(f"Element '{element}' not found.")
-
         elif choice == 4:
-            print("Current List:", my_list)
-        
+            print(f"Current list: {my_list}")
         elif choice == 5:
             my_list.sort()
-            print("Current List:", my_list)
-            
-        elif choice ==6:
+            print("List sorted.")
+        elif choice == 6:
             my_list.reverse()
-            print("Current List:", my_list)
-            
-            
-        
-
+            print("List reversed.")
         elif choice == 7:
-            print("Exiting...")
             break
-
         else:
-            print("Invalid choice! Please try again.")
+            print("Invalid choice. Please try again.")
 
-# Call the function to start the program
 list_operations()
